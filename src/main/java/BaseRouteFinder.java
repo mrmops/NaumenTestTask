d0804e3f3187e7b78449c386b568d4ea7d155a28
mrmops <kiriell.marshall@gmail.com>
2021-05-18 12:51:16 +0500

    Последние правки

--- src/main/java/BaseRouteFinder.java
+++ src/main/java/BaseRouteFinder.java
@@ -4,13 +4,17 @@ import java.util.Queue;
 
 public class BaseRouteFinder implements RouteFinder {
 
+    private Point[] shifts = new Point[]{
+            new Point(0, 1),
+            new Point(0, -1),
+            new Point(1, 0),
+            new Point(-1, 0)
+    };
+
     @Override
     public char[][] findRoute(char[][] map) {
         Point[] startTarget = getStart(map);
-        PathElement path = findPath(map, startTarget);
-        if (path == null)
-            return null;
-        return addPathToMap(map, startTarget[0], path.location);
+        return findPath(map, startTarget);
     }
 
     private Point[] getStart(char[][] map) {
@@ -31,47 +35,50 @@ public class BaseRouteFinder implements RouteFinder {
         throw new IllegalArgumentException("В входных данных нет старта!");
     }
 
-    private PathElement findPath(char[][] map, Point[] startTarget) {
+    private char[][] findPath(char[][] map, Point[] startTarget) {
         var start = startTarget[0];
         var target = startTarget[1];
-        long usedArrayBytes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        System.out.println(usedArrayBytes / 1048576d);
         int width = map.length;
         int height = map[0].length;
+
+        /*long usedArrayBytes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.println(usedArrayBytes / 1048576d);*/
+
+        byte[][] directions = new byte[width][];
+        for (int i = 0; i < width; i++)
+            directions[i] = new byte[height];
+
         SortedQueue<PathElement, Double> queue = new SortedQueue<>();
         PathElement startPath = new PathElement(start, 0);
         queue.enqueue(startPath, getLength(startPath, target));
-        map[start.getX()][start.getY()] = '@';
+
         while (queue.size() != 0) {
             PathElement current = queue.dequeue();
             Point currentPoint = current.getLocation();
-            for (var dy = -1; dy <= 1; dy++)
-                for (var dx = -1; dx <= 1; dx++) {
-                    if (Math.abs(dx) != Math.abs(dy)) {
-                        int newX = currentPoint.getX() + dx;
-                        int newY = currentPoint.getY() + dy;
-                        if (InMap(width, height, newX, newY)) {
-                            Point newPoint = new Point(newX, newY);
-                            char mapElement = map[newX][newY];
-                            if (mapElement == '.') {
-                                PathElement pathElement = new PathElement(newPoint, current.getCost() + 1);
-                                queue.enqueue(pathElement, getLength(pathElement, target));
-                                if (dx < 0)
-                                    map[newX][newY] = '>';
-                                else if (dx > 0)
-                                    map[newX][newY] = '<';
-                                else if (dy < 0)
-                                    map[newX][newY] = '˅';
-                                else
-                                    map[newX][newY] = '^';
-                            } else if (mapElement == 'X') {
-                                return current;
-                            }
-                        }
+            for (Point shift : shifts) {
+                int newX = currentPoint.getX() + shift.getX();
+                int newY = currentPoint.getY() + shift.getY();
+                if (InMap(width, height, newX, newY)) {
+                    Point newPoint = new Point(newX, newY);
+                    char mapElement = map[newX][newY];
+                    if (mapElement == '.' && directions[newX][newY] == 0) {
+                        PathElement pathElement = new PathElement(newPoint, current.getCost() + 1);
+                        queue.enqueue(pathElement, getLength(pathElement, target));
+                        if (shift.getX() < 0)
+                            directions[newX][newY] = 6;
+                        else if (shift.getX() > 0)
+                            directions[newX][newY] = 4;
+                        else if (shift.getY() < 0)
+                            directions[newX][newY] = 2;
+                        else
+                            directions[newX][newY] = 8;
+                    } else if (mapElement == 'X') {
+                        return addPathToMap(map, directions, start, currentPoint);
                     }
                 }
-           /*long usedBytes2 = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() - usedArrayBytes;
-           System.out.println(usedBytes2 / 1048576d);*/
+            }
+            /*usedBytes2 = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() - usedArrayBytes;
+            System.out.println(usedBytes2 / 1048576d);*/
             //Main.printMap(map);
         }
 
@@ -89,18 +96,18 @@ public class BaseRouteFinder implements RouteFinder {
         return Math.abs(dx) + Math.abs(dy) + startPath.cost;
     }
 
-    private char[][] addPathToMap(char[][] map, Point start, Point target) {
-        Point currentPoint = target;
+    private char[][] addPathToMap(char[][] map, byte[][] directions, Point start, Point pathStart) {
+        Point currentPoint = pathStart;
         while (!currentPoint.equals(start)) {
             int newX = currentPoint.getX();
             int newY = currentPoint.getY();
-            char symbol = map[newX][newY];
+            byte symbol = directions[newX][newY];
             map[newX][newY] = '+';
-            if (symbol == '>') {
+            if (symbol == 6) {
                 newX += 1;
-            } else if (symbol == '<') {
+            } else if (symbol == 4) {
                 newX -= 1;
-            } else if (symbol == '^') {
+            } else if (symbol == 8) {
                 newY -= 1;
             } else {
                 newY += 1;
@@ -164,16 +171,9 @@ public class BaseRouteFinder implements RouteFinder {
             return y;
         }
 
-        public void setY(int y) {
-            this.y = y;
-        }
-
         public int getX() {
             return x;
         }
 
-        public void setX(int x) {
-            this.x = x;
-        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,36 +6,42 @@ public class Main {
 
     public static void main(String[] args) {
         RouteFinder finder = new BaseRouteFinder();
-        List<char[]> mapList = new ArrayList<>();
+        /*List<char[]> mapList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-//        while (true) {
-//            String line = sc.nextLine();
-//            if (line.isEmpty())
-//                break;
-//            mapList.add(line.toCharArray());
-//        }
+        while (true) {
+            String line = sc.nextLine();
+            if (line.isEmpty())
+                break;
+            mapList.add(line.toCharArray());
+        }
+        mapList.toArray(map);*/
+
         int i = 10000;
+        char[][] map = new char[i][];
+        for (int x = 0; x < i; x++) {
+            char[] line = new char[i];
+            for (int y = 0; y < i; y++) {
+                line[y] = '.';
+            }
+            map[x] = line;
+        }
+
+        map[0][0] ='@';
+        map[i - 1][i - 1] ='X';
+
+        /*int i = 10000;
         char[][] map = new char[i][];
         map[0] = "...@........................................................................####".toCharArray();
         map[1] = ".###########################################################################....".toCharArray();
         for (int x = 2; x < i; x++) {
-            /*char[] line = new char[i];
-            for (int y = 0; y < i; y++) {
-                line[y] = '.';
-            }
-            map[x] = line;*/
             map[x] = ".............................................................................#..".toCharArray();
         }
 
         map[i - 2] = "###############################################################################.".toCharArray();
-        map[i - 1] = ".X..............................................................................".toCharArray();
-/*        map[0][0] ='@';
-        map[i - 1][i - 1] ='X';*/
-
-//        mapList.toArray(map);
+        map[i - 1] = ".X..............................................................................".toCharArray();*/
 
         char[][] map2 = finder.findRoute(map);
-        sc.nextLine();
+        //sc.nextLine();
         printMap(map2);
     }
 
